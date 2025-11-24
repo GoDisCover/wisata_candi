946a043b991fa9da613bd9e86b5e7a506106c3b9
GoDisCover <jessenco122@gmail.com>
2025-11-24 17:42:17 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/home_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
 import 'package:wisata_candi/screens/search_screen.dart';
 import 'package:wisata_candi/screens/sign_in_screen.dart';
@@ -39,7 +40,8 @@ class MainApp extends StatelessWidget {
       // home: ProfileScreen(),
       // home: SignInScreen(),
       // home: SignUpScreen(),
-      home: SearchScreen(),
+      // home: SearchScreen(),
+      home: HomeScreen(),
     );
   }
 }
